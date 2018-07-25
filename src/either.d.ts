44ac7be54b584b56fbed43ca88faf4052d6ee005
Avalander <darth.quesorico@gmail.com>
2018-07-25 08:48:55 +0200

    Refine typings.

--- src/either.d.ts
+++ src/either.d.ts
@@ -1,11 +1,8 @@
 export as namespace either
 
-type EitherMap<R> = (value: R) => R
-type EitherChain<L, R> = (value: R) => Either<L, R>
-
 export interface Either<L, R> {
-	map(fn: EitherMap<R>): Either<L, R>
-	chain(fn: EitherChain<L, R>): Either<L, R>
+	map(fn: (value: R) => R): Either<L, R>
+	chain(fn: (value: R) => Either<L, R>): Either<L, R>
 	fold(
 		fnLeft: (L) => any,
 		fnRight: (R) => any
@@ -17,6 +14,6 @@ export function Left<L, R>(value: L): Either<L, R>
 
 export function Right<L, R>(value: R): Either<L, R>
 
-export function tryCatch<L, R>(fn: () => any, ...args: []): Either<L, R>
+export function tryCatch<L, R>(fn: () => any, ...args: Array<any>): Either<L, R>
 
 export function conditional<L, R>(fn: (R) => boolean, reason: L): (value: R) => Either<L, R>
--- src/maybe.d.ts
+++ src/maybe.d.ts
@@ -1,11 +1,8 @@
 export as namespace maybe
 
-type MaybeMap<T> = (T) => T
-type MaybeChain<T> = (T) => Maybe<T>
-
 export interface Maybe<T> {
-	map(fn: MaybeMap<T>): Maybe<T>
-	chain(fn: MaybeChain<T>): Maybe<T>
+	map(fn: (value: T) => T): Maybe<T>
+	chain(fn: (value: T) => Maybe<T>): Maybe<T>
 	fold(
 		fnLeft: () => any,
 		fnRight: (T) => any
@@ -19,4 +16,4 @@ export function Nothing(): Maybe<any>
 
 export function fromNullable<T>(value: T): Maybe<T>
 
-export function fromList<T extends []>(value: T): Maybe<T>
+export function fromList<T extends Array<any>>(value: T): Maybe<T>
