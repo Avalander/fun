5ab7b83f00f2c1cd60c7dc16409e0748254a9880
Avalander <darth.quesorico@gmail.com>
2018-08-03 19:03:06 +0200

    Fix union to work without spread operator.

--- src/union.js
+++ src/union.js
@@ -1,10 +1,13 @@
 module.exports.union = types =>
-    types.reduce((prev, type) => ({
-        ...prev,
-        [type]: data => ({
-            match: fns => fns[type](data),
-        }),
-	}), {})
+    types.reduce((prev, type) =>
+        Object.assign({},
+            prev,
+            { [type]: data =>
+                ({
+                    match: fns => fns[type](data),
+                }),
+            }
+        ), {})
 
 module.exports.matcher = type => data =>
 	({
